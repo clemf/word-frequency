083165a2a77f08d61b557adc2ce9e18bb39f0424
Clem <clemfree@gmail.com>
2015-01-09 11:31:22 -0800

    Add basic functionality to Sinatra app

--- app.rb
+++ app.rb
@@ -1,11 +1,13 @@
 require 'sinatra'
 require 'sinatra/reloader'
-also_reload './lib/word_freq'
+also_reload '/lib/**/*.rb'
+require './lib/word_freq'
 
 get '/' do
 	erb :word_freq
 end
 
 get '/results' do
+	@result_num = params.fetch('search_target').word_freq(params.fetch('input_word'))
 	erb :results
 end

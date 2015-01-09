1af538c6382293024350bd4fdbfb8ed401edcaf4
Clem <clemfree@gmail.com>
2015-01-09 10:22:38 -0800

    Add spec for finding words in string

--- spec/word_freq_spec.rb
+++ spec/word_freq_spec.rb
@@ -1,9 +1,12 @@
-require "rspec"
-require "word_freq"
-require "pry"
+require 'rspec'
+require 'word_freq'
+require 'pry'
 
-describe "String#word_freq" do
-	it "returns 0 if the word is not found" do
-		expect("foo".word_freq("bar")).to eq 0
+describe 'String#word_freq' do
+	it 'returns 0 if the word is not found' do
+		expect('foo'.word_freq('bar')).to eq 0
+	end
+	it 'returns the number of times input word is found' do
+		expect('foo bar foo bar').word_freq('bar')).to eq 2
 	end
 end
